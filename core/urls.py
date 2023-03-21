bbae080f5da38b1a031741da61f443bdefaa38b5
Ripeplantain <irvingmanny@gmail.com>
2023-03-21 10:05:42 +0000

    able to update products

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin'),
     path('delete/<int:id>/', views.delete_product, name='delete'),
     path('create-product/', views.create_product, name='create-product'),
+    path('update-product/<int:id>/', views.update_product, name='update-product'),
 ]
--- core/views.py
+++ core/views.py
@@ -73,3 +73,21 @@ def create_product(request):
             return redirect('admin')
     else:
         return render(request, 'core/create_product.html', {'form': form})
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def update_product(request, id):
+    """
+    This view will update a product in the database
+    """
+    product = Product.objects.get(id=id)
+    form = ProductForm(instance=product)
+     
+    if request.method == 'POST':
+        form = ProductForm(request.POST,request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product updated successfully')
+            return redirect('admin')
+    else:
+        return render(request, 'core/update_product.html', {'form': form})
