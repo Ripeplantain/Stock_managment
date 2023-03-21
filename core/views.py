aa7fe71d37597c367e13539e899fceac21a604cd
Ripeplantain <irvingmanny@gmail.com>
2023-03-21 02:12:40 +0000

    can view products

--- core/views.py
+++ core/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Product
+
 def dashboard(request):
-    return render(request, 'core/dashboard.html')
+    """
+    This view will render the dashboard page
+    """
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+    return render(request, 'core/dashboard.html',context)
